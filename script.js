eb6fd1c01bb5ad6e2757d955b5c79369e5c4a982
mariadelapazschibli-spec <mariadelapaz.schibli@gmail.com>
2026-03-12 00:32:33 +0100

    Update script.js
    function collide: Der Dino kippt um 90 Grad, sobald er mit dem Stein in Berührung kommt. 
    
    function resetDino: Er dreht sich wieder zurück, sobald der Stein wieder am Anfang ist. Ohne diese Funktion würde der Dino liegen bleiben, was im Spiel keinen Sinn machen würde. 
    
    -> Änderungen sind auch im Code mit Kommentaren vermerkt.

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ function jump() {
   setTimeout(() => dino.classList.remove('jump-animation'), 500)
 }
 
+/* neu: Funktion collide beschreibt Animation bei Kollision */
+function collide() {
+  dino.style.transform = "rotate(-90deg)"
+}
+
+/* neu: Funktion resetDino macht Dino Animation rückgängig */
+function resetDino() {
+  dino.style.transform = "rotate(0deg)"
+}
+
 document.addEventListener('keypress', (event) => {
   if (!dino.classList.contains('jump-animation')) {
     jump()
@@ -22,13 +32,17 @@ setInterval(() => {
   )
   score.innerText++
 
+/* neu: Funktion resetDino wird ausgelöst*/
   if (rockLeft < 0) {
     rock.style.display = 'none'
+    resetDino()
   } else {
     rock.style.display = ''
   }
 
+/* neu: Funktion collide wird ausgelöst */
   if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
     score.innerText = 0
+    collide()
   }
 }, 25)
